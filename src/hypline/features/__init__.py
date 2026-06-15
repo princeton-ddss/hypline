e595ee609dddd90ee16be5bbfee3b0eb6a908c5a
Sangyoon Park <datumvitae@gmail.com>
2026-06-15 17:13:15 -0400

    Export SpectralFeature from features package

--- src/hypline/features/__init__.py
+++ src/hypline/features/__init__.py
@@ -1,7 +1,9 @@
 from .phonemic import PhonemicFeature
 from .semantic import SemanticFeature
+from .spectral import SpectralFeature
 
 __all__ = [
     "PhonemicFeature",
     "SemanticFeature",
+    "SpectralFeature",
 ]
